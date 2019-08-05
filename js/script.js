959c11694cffc3bdbb51ddc8d0c8a07b63b4b1df
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-05 12:39:14 +0200

    i have added some script codes

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ this.toppings= topping;
 this.crusts= crust;
 }
 var s = ["small", "medium", "large"];
-var t = ["Tomato sauce", "meat", "cheese","green pepper", "baby fresh spinach", "baby fresh spinach", "garlic", "broccoli"]
+var t = ["Tomato sauce", "meat", "cheese","green pepper", "baby fresh spinach", "baby fresh spinach", "garlic", "broccoli", "jambo", "saucage", "red pepper"]
+var c = ["Cripsy", "Stuffed", "Clutten", "Thin", "Multi-grain"];
