c84711e4e0d4f30944bfc3ed5b008beb9c355374
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-05 12:34:52 +0200

    i have added some script codes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function submitById(size, toppings, crusts){
+this.size= size;
+this.toppings= topping;
+this.crusts= crust;
+}
+var s = ["small", "medium", "large"];
+var t = ["Tomato sauce", "meat", "cheese","green pepper", "baby fresh spinach", "baby fresh spinach", "garlic", "broccoli"]
